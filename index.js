9f575fff8c28b574829507d98b1af8ce33dbfd92
priiyam <crifte@gmail.com>
2017-12-12 23:24:55 +0530

    connecting client product build with backend server

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const config = require('./config/database');
+const path = require('path');
 
 mongoose.Promise = global.Promise;
 mongoose.connect(config.uri, (err)=> {
@@ -11,8 +12,11 @@ mongoose.connect(config.uri, (err)=> {
         console.log('Connected to database: ' + config.db);
     }
 });
+
+app.use(express.static(__dirname + '/client/dist/'));
+
 app.get('*', (req, res) => {
-    res.send('hello world');
+    res.sendFile(path.join(__dirname + '/client/dist/index.html'));
 });
 
 app.listen(8080, () => {
