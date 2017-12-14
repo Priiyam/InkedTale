ab371d1fa1dc466b1ae0ac3a9bbc6635fbfba006
priiyam <crifte@gmail.com>
2017-12-14 22:27:42 +0530

    updated user.js, authentication.js, added body parser

--- models/user.js
+++ models/user.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
 const userSchema = new Schema({
     email: { type: String, required: true, unique: true, lowercase: true},
