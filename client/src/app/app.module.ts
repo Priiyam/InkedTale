871e7a79f2b9c4f62dd0eb5f44f95c0cf9d60ca5
priiyam <crifte@gmail.com>
2017-12-17 17:29:09 +0530

    implemented reactive forms

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
@@ -17,6 +18,7 @@ import { RegisterComponent } from './components/register/register.component';
   ],
   imports: [ //modules
     BrowserModule,
+    ReactiveFormsModule,
     AppRoutingModule
   ],
   providers: [], //services
