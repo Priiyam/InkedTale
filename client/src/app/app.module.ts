0c0970a3e9239e69a9277fa2adf022f89a5014a0
priiyam <crifte@gmail.com>
2017-12-19 19:19:29 +0530

    added http service for f/b communication

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/register/register.component';
+import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [ //components
@@ -21,7 +23,7 @@ import { RegisterComponent } from './components/register/register.component';
     ReactiveFormsModule,
     AppRoutingModule
   ],
-  providers: [], //services
+  providers: [AuthService], //services
   bootstrap: [AppComponent]
 })
 export class AppModule { }
