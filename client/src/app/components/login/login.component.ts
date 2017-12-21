a8cdc92f8f633584c5d970a09c34c0e03249fece
priiyam <crifte@gmail.com>
2017-12-21 21:14:00 +0530

    create form, disable and enable form methods

--- client/src/app/components/login/login.component.ts
+++ client/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  message;
+  messageClass;
+  processing = false;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {}
+
+  createForm(){
+    this.form = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  disableForm(){
+    this.form.controls["username"].disable();
+    this.form.controls["password"].disable();
+  }
+
+  enableForm(){
+    this.form.controls["username"].enable();
+    this.form.controls["password"].enable();
+  }
+
+ 
 
   ngOnInit() {
   }
